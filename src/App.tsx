b75de98d6aa5b178fc16b462d885ed00d3cffca2
Allen Kung <ak2253@njit.edu>
2020-11-15 19:24:21 -0500

    Add tsx template for login, signup and mainmenu.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
 import './App.css';
+import Login from './Login';
+import SignUp from './SignUp';
+import MainMenu from './MainMenu';
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Route exact path="/" render={() => (
+          <Redirect to="/login"/>
+        )}/>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/mainmenu">
+            <MainMenu />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
